a823d7d0cbcc8bdcba2f2424ca99ad7ce3c8710d
saidsahinbas <saidsahinbas@gmail.com>
2024-02-27 15:03:28 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-const fn = function () {
-    console.log('uygulama');
-}
+const express = require('express');
+const path = require('path');
+const app = express();
+/*
+const myLogger = (req, res, next) => {
+    console.log('Middleware log 1');
+    next();
+} 
+*/
 
-fn();
+//Middlewares
+//app.use(myLogger);
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+
+    /*const photo = {
+        id: 1,
+        name:"said photo",
+        description: "Photo desc"
+    }
+    res.send(photo);
+    */
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı`);
+})
